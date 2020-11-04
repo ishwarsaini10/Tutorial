3e064695c417a21b0eb3300abe716fb24939dc14
ishwarsaini10 <ishwarsaini10@gmail.com>
2020-11-04 18:39:03 +0530

    A new code added

--- series_04_11_2020.C
+++ series_04_11_2020.C
@@ -1,9 +1,9 @@
 
-
 /*
 Name  - Ishwar Singh
 Aim   - Error analysis in series sum.
 Email - ishwarsaini10@gmail.com
+Date  - 04.11.2020
 */
 
 /*C++ Directives*/
@@ -19,10 +19,10 @@ Email - ishwarsaini10@gmail.com
 #include<TGraph.h>
 #include<TH1F.h>
 
-float series1(int n){
+double series1(int n){
     int   i ;
-    float t ;
-    float s ;
+    double t ;
+    double s ;
     s = t = -1.0/2.0  ;
     for(i=1;  i<2*n;  i++){
         t*=-(pow((i+1),2))/(i*(i+2))  ;
@@ -31,12 +31,12 @@ float series1(int n){
     return(s);
 }
 
-float series2(int n){
+double series2(int n){
     int   i   ;
-    float t1  ;
-    float t2  ;
-    float s1  ;
-    float s2  ;
+    double t1  ;
+    double t2  ;
+    double s1  ;
+    double s2  ;
     s1  = t1  = 1.0/2.0   ;
     s2  = t2  = 2.0/3.0   ;
     for(i=1;  i<n;  i++){
@@ -48,10 +48,10 @@ float series2(int n){
     return(s2-s1);
 }
 
-float series3(int n){
+double series3(int n){
     int   i ;
-    float t ;
-    float s ;
+    double t ;
+    double s ;
     s = t = 1.0/6.0  ;
     for(i=1;  i<n;  i++){
         t*=(n*(2*n+1))/((n+1)*(2*n+3));
@@ -59,37 +59,28 @@ float series3(int n){
     }
     return(s);
 }
-int series_v3(){
+int series_04_11_2020(){
     const int N = 10000     ;
     const int Nbins = 1000;       
-    float     x_min = 0.0 ;
-    float     x_max = 6.0 ;
-    float     S1          ;
-    float     n           ;      
-    float s1_final        ;
-    float s2_final        ;
-    float s3_final        ;
+    double     x_min = 0.0 ;
+    double     x_max = 6.0 ;
+    double     S1          ;
+    double     n           ;      
+    double s1_final        ;
+    double s2_final        ;
+    double s3_final        ;
     std::ofstream myfile  ;
     myfile.open("series_04_11_2020.txt");
     std::cout <<  std::setprecision(7) << std::setw(10) ;
-
-    //TGraph  *graph  = new TGraph(N, );         
-//    TH1F *hist  = new TH1F("hist", "Error Analysis",  Nbins,  x_min,  x_max);
 
     for(int j=1;  j<=N; j++){
         s1_final  = series1(j);
         s2_final  = series2(j);
         s3_final  = series3(j);
-        //std::cout <<  s1_final  <<  "\t"  <<  s2_final  <<  "\t"  <<  s3_final  <<  std::endl;
-        myfile <<  s1_final  <<  "\t"  <<  s2_final  <<  "\t"  <<  s3_final  <<  std::endl;
-/*
-        S1 = TMath::Log10(TMath::Abs((s1_final-s3_final)/s3_final));
-        n  = TMath::Log10(j);  
-        hist  ->  Fill(n, S1);
-  */
+        myfile <<  j <<  "\t"  <<  TMath::Log10(TMath::Abs((s1_final-s3_final)/s3_final))  <<  std::endl;
+  
         }
     
-//    hist  ->  Draw();
 
     return 0;
 }
